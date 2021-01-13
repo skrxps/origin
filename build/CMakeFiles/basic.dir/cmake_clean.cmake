faf03245381da37337066d3ea2f390c043d07f59
skrxps <skrxps@outlook.com>
2021-01-13 14:51:22 -0600

    main

---
+++ build/CMakeFiles/basic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic.dir/main.cpp.obj"
+  "basic.exe"
+  "basic.exe.manifest"
+  "basic.pdb"
+  "libbasic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/codex/workspace/workspacec
+# Build directory: D:/codex/workspace/workspacec/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
