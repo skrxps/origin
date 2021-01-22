362315aa39d98f071ef45327320ad9a8e5d8a652
skrxps <skrxps@outlook.com>
2021-01-22 01:30:08 -0600

    main

---
+++ src/leetcode/leetcode0020.c
@@ -0,0 +1,45 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+char pairs(char a) {
+    if (a == '}') return '{';
+    if (a == ']') return '[';
+    if (a == ')') return '(';
+    return 0;
+}
+
+bool isValid(char* s) {
+    int n = strlen(s);
+    if (n % 2 == 1) {
+        return false;
+    }
+    int stk[n + 1], top = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        char ch = pairs(s[i]);
+        if (ch) {
+            if (top == 0 || stk[top - 1] != ch) {
+                return false;
+            }
+            top--;
+        } else {
+            stk[top++] = s[i];
+        }
+    }
+    return top == 0;
+}
+
+int main()
+{
+    char *s = "{(})";
+    if(isValid(s))
+    {
+        printf("true\n");
+    }
+    if(!isValid(s))
+    {
+        printf("false\n");
+    }
+    return 0;
+}
